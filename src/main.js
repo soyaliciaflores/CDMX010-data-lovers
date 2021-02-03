683f0289bae0b51d63e4812673f43c36ff8cdb94
MiriamNM <miriamnavamucino@gmail.com>
2021-02-03 12:47:25 -0600

    New chages

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
 import {theAthletes} from './data/athletes/athletes.js';
 import { cards, showCards } from './data.js';
 
-export const butShowAll= document.getElementById("buttonShowAll");
-butShowAll.addEventListener ("click", function showCards() {
-    
-         
-    
-    document.getElementById("resultados").innerHTML= cardsFunction
-});
 
+export let cards = (athlete) => {
+  let card = `
+  <div class="cardAthletes">
+  <img class="logochico" src="assets/logo rio de janeiro.png">
+  <h1 class="name">Name: ${athlete.name}</h1>
+  <h2>Gender: ${athlete.gender}</h2>
+  <h2>Height: ${athlete.height}</h2>
+  <h2>Weight: ${athlete.weight}</h2>
+  <h2>Sport: ${athlete.sport}</h2>
+  <h2>Team: ${athlete.team}</h2>
+  <h2>Age: ${athlete.age}</h2>
+  <h2>Event: ${athlete.event}</h2>
+  <h2>Medal: ${athlete.medal}</h2>
+  </div>
+  `;
+  return card;
+}
+
+const butShowAll= document.getElementById("buttonShowAll");
+butShowAll.addEventListener ("click", function showCards() {
+      
+    const losAtletas = theAthletes.athletes;
+    let cardsFunction = losAtletas.map((athlete)=>{
+        return  cards(athlete);
+        }).join(" ");
+
+    document.getElementById("resultados").innerHTML= cardsFunction
+      };
+         
 // ya aniadi las variables
 
 const selectGender = document.querySelector("#genderFilter");
