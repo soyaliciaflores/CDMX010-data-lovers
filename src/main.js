1c241779dbb333e399fdbe3ef25364e9c9aff51b
AnaMartinez040816 <75271833+AnaMartinez040816@users.noreply.github.com>
2021-02-03 20:42:25 -0600

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import {theAthletes} from './data/athletes/athletes.js';
-
 const losAtletas= theAthletes.athletes
 const butShowAll= document.getElementById("buttonShowAll");
 butShowAll.addEventListener ("click", function showCards() {
    let cardsFunction = losAtletas.map((e)=>{
        return cards(e);
    }).join(""); 
-
-
+  document.getElementById("resultados").innerHTML= cardsFunction
+});
 
 let cards = (athlete) => {
   let card = `
@@ -25,37 +24,6 @@ let cards = (athlete) => {
   </div>
   `;
   return card;
-}
-
-const butShowAll= document.getElementById("buttonShowAll");
-butShowAll.addEventListener ("click", function showCards() {
-      
-    const losAtletas = theAthletes.athletes;
-    let cardsFunction = losAtletas.map((athlete)=>{
-        return  cards(athlete);
-        }).join(" ");
-
-
-    document.getElementById("resultados").innerHTML= cardsFunction
-      };
-        
-
-let cards = (athlete) => {
-    let card = `
-    <div class="cardAthletes">
-    <img class="logochico" src="assets/logo rio de janeiro.png">
-    <h1 class="name">Name: ${athlete.name}</h1>
-    <h2>Gender: ${athlete.gender}</h2>
-    <h2>Height: ${athlete.height}</h2>
-    <h2>Weight: ${athlete.weight}</h2>
-    <h2>Sport: ${athlete.sport}</h2>
-    <h2>Team: ${athlete.team}</h2>
-    <h2>Age: ${athlete.age}</h2>
-    <h2>Event: ${athlete.event}</h2>
-    <h2>Medal: ${athlete.medal}</h2>
-    </div>
-    `;
-    return card;
 }
 
 const selectGender = document.querySelector("#genderFilter");
